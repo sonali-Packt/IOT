1bdefd11a9ea3d5beb1f71c3619f3a45b7a682c5
Anum Sheraz <anumsheraz1@hotmail.com>
2018-05-08 01:53:52 +0100

    S2-P1 Added heartbeat 'keep_alive' functionality

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+
+alive = 0
+data = {}
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
+@app.route('/keep_alive', methods=['GET'])
+def keep_alive():
+    global alive, data
+    alive += 1
+    keep_alive_count = str(alive)
+    data['keep_alive'] = keep_alive_count
+    parsed_json = json.dumps(data)
+    print(parsed_json)
+    return str(parsed_json)
 
 if __name__ == '__main__':
     app.run(port=80)
---
+++ static/js/main.js
@@ -0,0 +1,32 @@
+
+  var alive_second = 0;
+  var heartbeat_rate = 5000;
+
+  function keep_alive(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+      if(this.readyState === 4){
+            if (this.status === 200) {
+              if (this.responseText !== null) {
+                var date = new Date();
+                alive_second = date.getTime();
+                var keep_alive_data=this.responseText;
+                }
+            }
+        }
+    };
+    request.open("GET","keep_alive", true);
+    request.send(null);
+    setTimeout('keep_alive()', heartbeat_rate);
+   }
+
+  function time(){
+            var d = new Date();
+            var current_sec = d.getTime();
+            if (current_sec - alive_second > heartbeat_rate + 1000){
+               document.getElementById("Connection_id").innerHTML = " Dead";
+            } else {
+               document.getElementById("Connection_id").innerHTML = " Alive";
+            }
+    setTimeout('time()',1000);
+   }
