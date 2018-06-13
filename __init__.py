d28aae2fc65f83d0a2138943ec749201d5b4e1e8
root <root@ip-172-31-29-202.us-east-2.compute.internal>
2018-06-13 21:56:04 +0000

    S5-P3 Basic Facebook login Access

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session, flash
+from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
+
 import json, time
 
 app = Flask(__name__)
@@ -6,8 +8,47 @@ app = Flask(__name__)
 alive = 0
 data = {}
 
-@app.route('/')
-def hello():
+#Paste in your facebook app ID and secret
+facebookId = "180844045961238"
+facebookSecret = "da8512c089189c459af393ab7df8e37b"
+
+facebook_blueprint = make_facebook_blueprint(client_id=facebookId, client_secret=facebookSecret)
+app.register_blueprint(facebook_blueprint, url_prefix='/facebook_login')
+
+@app.route('/facebook_login')
+def facebook_login():
+    if not facebook.authorized:
+        return redirect(url_for('facebook.login'))
+
+    account_info = facebook.get('/me')
+    if account_info.ok:
+        print("access_token: ", facebook.access_token)
+        me = account_info.json()
+        session['facebook_token'] = facebook.access_token
+        session['user'] = me['name']
+        session['user_id'] = me['id']
+        return redirect(url_for('main'))
+
+    return redirect(url_for('login'))
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    session['facebook_token'] = None
+    session['user'] = None
+    session['user_id'] = None
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    session['facebook_token'] = None
+    session['user'] = None
+    session['user_id'] = None
+    flash("you just logged out!")
+    return redirect(url_for('login'))
+
+@app.route('/main')
+def main():
+    flash(session['user'])
     return render_template('index.html')
 
 @app.route('/keep_alive', methods=['GET'])
@@ -21,4 +62,4 @@ def keep_alive():
     return str(parsed_json)
 
 if __name__ == '__main__':
-	app.run(host="localhost", port=80)
+   app.run(host="localhost", port=80)
