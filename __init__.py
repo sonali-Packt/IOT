8638a5fd9ad93c95def68deb34561710b3639042
root <root@ip-172-31-29-202.us-east-2.compute.internal>
2018-06-19 00:05:34 +0000

    S5-P3 Added login_required

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, session, flash
 from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
+from functools import wraps
 
 import json, time
 
@@ -8,12 +9,13 @@ app = Flask(__name__)
 alive = 0
 data = {}
 
-#Paste in your facebook app ID and secret
+# Paste in your facebook app ID and secret
 facebookId = "180844045961238"
 facebookSecret = "da8512c089189c459af393ab7df8e37b"
 
 facebook_blueprint = make_facebook_blueprint(client_id=facebookId, client_secret=facebookSecret)
 app.register_blueprint(facebook_blueprint, url_prefix='/facebook_login')
+
 
 @app.route('/facebook_login')
 def facebook_login():
@@ -24,6 +26,7 @@ def facebook_login():
     if account_info.ok:
         print("access_token: ", facebook.access_token)
         me = account_info.json()
+        session['logged_in'] = True
         session['facebook_token'] = facebook.access_token
         session['user'] = me['name']
         session['user_id'] = me['id']
@@ -31,25 +34,47 @@ def facebook_login():
 
     return redirect(url_for('login'))
 
+
+def login_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if "logged_in" in session:
+            if session["logged_in"]:
+                return f(*args, **kwargs)
+
+        flash("please login first !")
+        return redirect(url_for('login'))
+
+    return wrapper
+
+
+def clear_user_session():
+    session['logged_in'] = None
+    session['facebook_token'] = None
+    session['user'] = None
+    session['user_id'] = None
+
+
 @app.route('/', methods=['POST', 'GET'])
 def login():
-    session['facebook_token'] = None
-    session['user'] = None
-    session['user_id'] = None
+    clear_user_session()
     return render_template('login.html')
 
+
 @app.route("/logout")
+@login_required  # you cannot logout if you're already not logged in!
 def logout():
-    session['facebook_token'] = None
-    session['user'] = None
-    session['user_id'] = None
+    clear_user_session()
     flash("you just logged out!")
     return redirect(url_for('login'))
 
+
 @app.route('/main')
+@login_required
 def main():
     flash(session['user'])
     return render_template('index.html')
+
 
 @app.route('/keep_alive', methods=['GET'])
 def keep_alive():
@@ -61,5 +86,6 @@ def keep_alive():
     print(parsed_json)
     return str(parsed_json)
 
+
 if __name__ == '__main__':
-   app.run(host="localhost", port=80)
+    app.run(host="localhost", port=80)
